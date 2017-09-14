1ff24e48334c7f5c662c7187a6cd1bf291d3f81e
David Berglund <dabe@river.se>
2017-09-14 11:12:38 +0200

    local sync by default. change to remote sync in local-slideshow/loop.sh
    Signed-off-by: David Berglund <dabe@river.se>

--- local_slideshow/loop.sh
+++ local_slideshow/loop.sh
@@ -3,5 +3,8 @@ while :
 do
 	echo "Press [CTRL+C] to stop.."
 	sleep 4
-        rsync -a user@xxx.xxx.xxx.xxx:imagefoldertosync/ . --delete
+        #local sync
+        rsync -avzh ../uploads/ uploads/ --delete
+        #or remote sync:
+        #rsync -a user@xxx.xxx.xxx.xxx:imagefoldertosync/ . --delete
 done
